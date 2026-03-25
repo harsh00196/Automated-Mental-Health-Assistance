d7e863e0acc80ad1cef0569d5670a8f486b6447b
Harsh Vardhan <harshvardhan@Harshs-MacBook-Air.local>
2026-03-25 09:38:36 +0530

    feat: Added Emergency Assistance Hub and Resource Viewing Modals

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000/api', 
+  baseURL: 'http://127.0.0.1:5000/api', // Flask API URL
 });
 
 api.interceptors.request.use((config) => {
